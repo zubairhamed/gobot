fa9369860e41a5f486dc7df33cc5b3d3a2660287
deadprogram <ron@hybridgroup.com>
2015-02-17 15:15:21 -0800

    Just some WIP on BMP180

--- platforms/i2c/bmp180_driver.go
+++ platforms/i2c/bmp180_driver.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"encoding/binary"
 	"time"
+	"math"
 
 	"github.com/hybridgroup/gobot"
 )
@@ -23,17 +24,17 @@ const BPM180_MODE_HIGHRES = 2
 const BPM180_MODE_UHIGHRES = 3
 
 type BPMCalibrationData struct {
-	ac1 int16
-	ac2 int16
-	ac3 int16
+	ac1 uint16
+	ac2 uint16
+	ac3 uint16
 	ac4 uint16
 	ac5 uint16
 	ac6 uint16
-	b1 int16
-	b2 int16
-	mb int16
-	mc int16
-	md int16
+	b1 uint16
+	b2 uint16
+	mb uint16
+	mc uint16
+	md uint16
 }
 
 type BMP180Driver struct {
@@ -42,9 +43,10 @@ type BMP180Driver struct {
 	interval      time.Duration
 	Calibration 	BPMCalibrationData
 	RawPressure 	uint16
-	Pressure   		int16
 	RawTemperature uint16
-	Temperature   int16
+	Pressure   		float64
+	Temperature   uint16
+	Altitude			uint16
 	gobot.Eventer
 }
 
@@ -150,7 +152,7 @@ func (h *BMP180Driver) readRawTemp() (err error, temperature uint16) {
 	return nil, h.RawTemperature
 }
 
-func (h *BMP180Driver) readRawPressure(mode uint8) (err error, pressure int16) {
+func (h *BMP180Driver) readRawPressure(mode uint8) (err error, pressure uint16) {
 	if err := h.connection.I2cWrite([]byte{BMP180_REGISTER_CONTROL, BMP180_REGISTER_READPRESSURECMD}); err != nil {
 		gobot.Publish(h.Event(Error), err)
 		return err, 0
@@ -171,10 +173,52 @@ func (h *BMP180Driver) readRawPressure(mode uint8) (err error, pressure int16) {
 	lsb:= ret[1]
 	xlsb := ret[2]
 
-  var rawPress int16
-  rawPress = int16(((msb << 16) + (lsb << 8) + xlsb) >> (8-mode))
-  return nil, rawPress
+  h.RawPressure = uint16(((msb << 16) + (lsb << 8) + xlsb) >> (8-mode))
+  return nil, h.RawPressure
 }
+
+func (h *BMP180Driver) calculateTemperature() uint16 {
+  x1 := ((h.RawTemperature - h.Calibration.ac6) * h.Calibration.ac5) >> 15;
+  x2 := (h.Calibration.mc << 11) / (x1 + h.Calibration.md)
+  b5 := x1 + x2
+  h.Temperature = ((b5 + 8) >> 4) / 10.0
+
+  return b5
+}
+
+func (h *BMP180Driver) calculatePressure(mode uint16, b5 uint16) {
+	var p float64
+	b6 := b5 - 4000
+	x1 := (h.Calibration.b2 * (b6 * b6) >> 12) >> 11
+	x2 := (h.Calibration.ac2 * b6) >> 11
+	x3 := x1 + x2
+	b3 := math.Ceil(float64((((h.Calibration.ac1 * 4 + x3) << mode) + 2) / 4))
+
+	x1 = (h.Calibration.ac3 * b6) >> 13
+	x2 = (h.Calibration.b1 * ((b6 * b6) >> 12)) >> 16
+	x3 = ((x1 + x2) + 2) >> 2
+	b4 := (h.Calibration.ac4 * (x3 + 32768)) >> 15
+	b7 := (h.RawPressure - uint16(b3)) * (50000 >> mode)
+
+	if (b7 < 0x80000000) {
+	    p = math.Ceil((b7 * 2) / b4)
+	} else {
+	    p = math.Ceil((b7 / b4) * 2)
+	}
+
+	x1 = (p >> 8) * (p >> 8)
+	x1 = (x1 * 3038) >> 16
+	x2 = (-7357 * p) >> 16
+
+	p = p + ((x1 + x2 + 3791) >> 4)
+
+	h.Pressure = p
+}
+
+func (h *BMP180Driver) calculateAltitude() {
+	
+}
+
 
 func waitTime(mode uint8) time.Duration {
 	switch mode {
